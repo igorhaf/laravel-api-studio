dbc48f7ff47797b54f9a153198c87110aa3a534f
Igor Herson <igorhaf@gmail.com>
2022-06-18 09:58:40 -0300

    feature maked

--- resources/js/components/HeadermenuComponent.vue
+++ resources/js/components/HeadermenuComponent.vue
@@ -4,6 +4,7 @@
             <li class="space-x-5 text-xl">
                 <router-link to="/" class="text-white hover:text-indigo-700 sm:inline-block"><svg class="code-icon"></svg></router-link>
                 <router-link to="/restful" class="text-white hover:text-indigo-700 sm:inline-block"><svg class="api-icon"></svg></router-link>
+                <router-link to="/database" class="text-white hover:text-indigo-700 sm:inline-block"><svg class="database-icon"></svg></router-link>
             </li>
         </ul>
     </nav>
